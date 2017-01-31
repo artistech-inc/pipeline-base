3733068c5b896f4ce06a5bad6f7baf2b15402656
Matthew Aguirre <matt.aguirre@gmail.com>
2017-01-31 10:05:43 -0500

    Updated to use base getKeys() method.

--- src/main/java/com/artistech/ee/beans/DataBase.java
+++ src/main/java/com/artistech/ee/beans/DataBase.java
@@ -5,8 +5,12 @@ package com.artistech.ee.beans;
 
 import com.artistech.utils.ExternalProcess;
 import java.io.File;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -96,7 +100,23 @@ public abstract class DataBase {
         return runPath.toArray(new String[]{});
     }
 
-    public abstract String[] getKeys();
+    public String[] getKeys() {
+        ArrayList<String> keys = new ArrayList<>();
+
+        Field[] fields = this.getClass().getFields();
+        for (Field f : fields) {
+            int modifiers = f.getModifiers();
+            if ((modifiers & (Modifier.STATIC | Modifier.FINAL))
+                    == (Modifier.STATIC | Modifier.FINAL)) {
+                try {
+                    keys.add(f.get(null).toString());
+                } catch (IllegalArgumentException | IllegalAccessException ex) {
+                    Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        return keys.toArray(new String[]{});
+    }
 
     public final ExternalProcess getProc() {
         return proc;
